fb2940182831dff61459a8c7c32287f50de921ba
Paul Anthony McGowan <xhostcom@gmail.com>
2020-04-30 13:40:28 +0100

    First Commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Hello Vite + Vue 3!</h1>
+  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
+  <p>
+    <span>Count is: {{ count }}</span>
+    <button @click="count++">increment</button>
+  </p>
+  <Index />
+</template>
+<script>
+//import Index from '@/components/Index.vue';
+export default {
+  //components: {
+   // Index
+  //},
+  data: () => ({ count: 0 }),
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #4fc08d;
+}
+
+h1, p {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ components/Index.vue
@@ -0,0 +1,12 @@
+<template>
+<h2>Hello New App!</h2>
+<h3>{{ world }}
+
+</template>
+<script>
+export default {
+    data: () => ({
+        world: "Hello World"
+    })
+}
+</script>
